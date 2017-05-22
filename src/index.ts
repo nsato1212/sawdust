21a405a3b5f5eb231469d370a8142ac3d83f82f1
noriki.satoh <nrkprd@gmail.com>
2017-05-23 03:51:31 +0900

    fix module resolution for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 
-import * as command from './command';
-import * as worker from './worker';
-import * as report from './report';
-
-export const Worker = worker.Worker;
-export const Command = command.Command;
-export const Report = report.Report;
+export * from './command';
+export * from './worker';
+export * from './report';
